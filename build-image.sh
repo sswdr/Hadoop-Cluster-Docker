9c7897f5ecc194392e3138b57530fe4712188f19
sswdr <771020712@qq.com>
2022-09-07 15:44:55 +0800

    init

--- build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ""
-
-echo -e "\nbuild docker hadoop image\n"
-sudo docker build -t sswdr/hadoop2.7.2:ubuntu20.04 .
-
-echo ""
--- tar/wget_hadoop-2.7.2.tar.gz.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/sswdr/Hadoop-Cluster-Docker/releases/download/hadoop-2.7.2.tar.gz/hadoop-2.7.2.tar.gz
